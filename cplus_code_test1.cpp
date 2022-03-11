1be3f50445ecdacc87f92c11239b7895b8db688e
AbrancoIST <andregomesbranco@tecnico.ulisboa.pt>
2022-03-11 00:06:25 +0000

    commit com compile já a funcionar

--- cplus_code_test1.cpp
+++ cplus_code_test1.cpp
@@ -5,6 +5,10 @@ int main()
 {
     std::string hello = "Hello World";
 
-    std::cout << hello << stf::endl;
+    std::cout << hello << std::endl;
 
+    int age = 0;
+    scanf("%d", &age);
+
+    return 0;
 }
